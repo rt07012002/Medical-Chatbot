f1735c1ae7607da469816b2e0e4bb4a671e0998d
Rounak Tanwar <140898380+rt07012002@users.noreply.github.com>
2026-04-03 15:13:08 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rounak Tanwar",
+    author_email="ue208089.rounak.it@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
